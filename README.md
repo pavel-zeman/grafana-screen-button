cb17dada46af89194ac6dbd6bf9f5b66d4b16edd
Pavel Zeman <pavel.zeman@email.cz>
2020-07-09 14:31:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Grafana screenshot helper
+Tampermonkey script, which adds button to remove overflows so that a screenshot of the whole page can be taken.
+
+To install follow these steps:
+1. Add https://tampermonkey.net/ into your browser
+2. Install User script from [script.js](https://github.com/pavel-zeman/grafana-screen-button/raw/master/script.js)
---
+++ script.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Grafana screenshot helper
+// @namespace    https://github.com/pavel-zeman/grafana-screen-button
+// @version      0.1
+// @author       Pavel Zeman
+// @include      http://*:3000/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  var link = $("<div style='cursor:pointer'>Screen</div>");
+  link.on("click", function() {
+    document.querySelectorAll("div.main-view > div.scroll-canvas")[0].style.setProperty("overflow", "visible", "important");
+    document.getElementsByClassName("scroll-canvas--dashboard")[0].style.setProperty("overflow", "visible", "important");
+    document.getElementsByClassName("custom-scrollbar--page")[0].style.setProperty("overflow", "visible", "important");
+    document.querySelectorAll("div.custom-scrollbar--page > div.view")[0].style.setProperty("overflow", "visible", "important");
+  });
+  link.insertBefore($(".sidemenu-item").first())
+})();
